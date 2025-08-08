d2c903fea2579249932852f9e73ae290f0ec41bf
lassereinboeng <lasseweinbrandt@gmail.com>
2025-08-08 16:10:06 +0200

    chore: Reformat the favicons.php file

--- site/snippets/components/favicons.php
+++ site/snippets/components/favicons.php
@@ -2,45 +2,31 @@
 /** @var Kirby\Cms\Site $site */
 if ($favicon = $site->favicon()->toFile()): ?>
 
-<link
-  rel="icon"
+<link rel="icon"
   type="image/png"
   sizes="32x32"
-  href="<?= $favicon->resize(32)->url() ?>"
-/>
-<link
-  rel="icon"
+  href="<?= $favicon->resize(32)->url() ?>" />
+<link rel="icon"
   type="image/png"
   sizes="16x16"
-  href="<?= $favicon->resize(16)->url() ?>"
-/>
-<link
-  rel="icon"
+  href="<?= $favicon->resize(16)->url() ?>" />
+<link rel="icon"
   type="image/png"
   sizes="96x96"
-  href="<?= $favicon->resize(96)->url() ?>"
-/>
-<link
-  rel="alternate icon"
+  href="<?= $favicon->resize(96)->url() ?>" />
+<link rel="alternate icon"
   type="image/png"
-  href="<?= $favicon->resize(180)->url() ?>"
-/>
-<link
-  rel="apple-touch-icon"
+  href="<?= $favicon->resize(180)->url() ?>" />
+<link rel="apple-touch-icon"
   type="image/png"
-  href="<?= $favicon->resize(180)->url() ?>"
-/>
-<link
-  rel="apple-touch-icon"
+  href="<?= $favicon->resize(180)->url() ?>" />
+<link rel="apple-touch-icon"
   type="image/png"
   sizes="167x167"
-  href="<?= $favicon->resize(167)->url() ?>"
-/>
-<link
-  rel="apple-touch-icon"
+  href="<?= $favicon->resize(167)->url() ?>" />
+<link rel="apple-touch-icon"
   type="image/png"
   sizes="152x152"
-  href="<?= $favicon->resize(152)->url() ?>"
-/>
+  href="<?= $favicon->resize(152)->url() ?>" />
 
 <?php endif ?>
