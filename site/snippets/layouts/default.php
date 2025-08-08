73e5ace942664834ffb65ffe19eabf8d6a9200c3
lassereinboeng <lasseweinbrandt@gmail.com>
2025-08-08 16:00:37 +0200

    fix: Add a emoji icon to silence favicon.ico error

--- site/snippets/layouts/default.php
+++ site/snippets/layouts/default.php
@@ -6,24 +6,30 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="<?= $site->lang() ?>" class="antialiased">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<html lang="<?= $site->lang() ?>"
+  class="antialiased">
 
-        <?php snippet('seo/head'); ?>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible"
+    content="IE=edge" />
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0" />
 
-        <?php snippet('components/favicons'); ?>
+  <?php snippet('seo/head'); ?>
+  <link rel="icon"
+    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>">
+  <?php snippet('components/favicons'); ?>
 
-        <?= vite(['src/main.css', 'src/main.ts']) ?>
-    </head>
+  <?= vite(['src/main.css', 'src/main.ts']) ?>
+</head>
 
-    <body class="<?= $page->template() ?>">
-        <main>
-            <?= $slot ?>
-        </main>
+<body class="<?= $page->template() ?>">
+  <main>
+    <?= $slot ?>
+  </main>
 
-        <?php snippet('seo/schemas'); ?>
-    </body>
+  <?php snippet('seo/schemas'); ?>
+</body>
+
 </html>
